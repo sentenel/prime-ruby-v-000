15ad904d5601c3d05be108527abe9675874fc6b3
Seth Wax <sethlwax@gmail.com>
2018-03-27 18:40:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2 || number % 2 == 0 || number % 3 == 0
+  i = 5
+  while i**2 < number
+    return false if number % i == 0
+    i += 6
+  end
+end
