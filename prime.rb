786fcaed4513f95ed74e5aa7f3ba9471611131d9
Seth Wax <sethlwax@gmail.com>
2018-03-27 18:44:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   is_prime = false if number < 2 || (number > 2 && number % 2 == 0) || number % 3 == 0
   i = 5
   while i**2 < number
-    is_prime = false if number % i == 0
+    is_prime = false if number % i == 0 || number % (i+2) == 0
     i += 6
   end
   is_prime
